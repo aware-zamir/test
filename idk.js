ae604af48ed1df864cd1e835a7adcf69c6087439
root <ali.hidari.777@gmail.com>
2025-02-16 17:24:01 +0330

    38

--- idk.js
+++ idk.js
@@ -1,1 +1,15 @@
-class
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('#redButton');
+    
+    // Add click event listener to the button
+    button.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
+
+// Note: Make sure you have this HTML in your idk.html file:
+/*
+<button id="redButton" style="background-color: red; color: white; padding: 10px 20px; border: none; cursor: pointer;">
+    Click Me
+</button>
+*/
